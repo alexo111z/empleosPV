905b54a651dcc05d3fa50e0a0f0c95c175821da6
alexo111z <alexo111y@gmail.com>
2019-11-29 23:05:24 -0600

    admins: plantillas

--- resources/views/admins/empresas.blade.php
+++ resources/views/admins/empresas.blade.php
@@ -4,22 +4,24 @@
 
     <main role="main" class="col-md-12 py-0 px-0">
         
-        <nav class="nav-buscador  flex-md-nowrap mt-6">
-            <div class="row div-search input-group search-group text-center pt-2 mx-auto w-50 mb-1">
-                <input class="form-control mx-auto form-control-dark " type="text" placeholder="Nombre empresa" aria-label="Search">
-                <div class="input-group-prepend">
-                    <button class="btn btn-search my-sm-0" type="submit"><i class="fas fa-search" aria-hidden="true"></i></button>
-                </div>
-            </div>
-        </nav>
         
-        <div class="data-table col-md-10 ">
-                <div class="componets">
-                    <button class="btn btn-primary">
-                        Agregar empresa    
-                        <i class="fa fa-plus"></i>
-                    </button>
+        <div class="conten col-md-10 ">
+                <div class="buscador">
+                    <div class="div-buscador  flex-md-nowrap mt-6">
+                        <div class="row div-search input-group search-group text-center pt-2 w-50 mb-1">
+                            <input class="form-control form-control-dark " type="text" placeholder="Nombre empresa" aria-label="Search">
+                            <div class="input-group-prepend">
+                                <button class="btn btn-search my-sm-0" type="submit"><i class="fas fa-search" aria-hidden="true"></i></button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <div class="componet">
+                        <button class="btn btn-primary add">
+                            Agregar empresa    
+                            <i class="fa fa-plus"></i>
+                        </button>
+                    </div>
             <table class="table data">
                 <thead>
                     <tr>
